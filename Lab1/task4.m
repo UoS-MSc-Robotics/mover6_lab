ea1b7e8a1a78e3141583e178270f868c0e8f56ba
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2023-11-21 13:39:00 +0000

    finished lab1

--- Lab1/task4.m
+++ Lab1/task4.m
@@ -1,11 +1,20 @@
 % Task 4
 mover6 = importrobot('CPMOVER6.urdf');
 
-randConfig = randomConfiguration(mover6);
-
 ik = inverseKinematics('RigidBodyTree', mover6);
 weights = [0.25 0.25 0.25 1 1 1];
+tform = getTransform(mover6, mover6.homeConfiguration, 'link6', 'base_link');
 
 initialguess = mover6.homeConfiguration;
-[configSoln, solnInfo] = ik('link6', trvec2tform([0.5 0.5 0.5]), weights, initialguess)
+[configSoln, solnInfo] = ik('link6', tform, weights, initialguess);
 
+% Display solnInfo using homeConfiguration
+disp("Home Configuration");
+disp(solnInfo);
+
+initialguess = randomConfiguration(mover6);
+[configSoln, solnInfo] = ik('link6', tform, weights, initialguess);
+
+% Display solnInfo using randomConfiguration
+disp("Random Configuration");
+disp(solnInfo);
